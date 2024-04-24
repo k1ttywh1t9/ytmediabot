8466fec52f0cc67804038062d5fabc6aac70e083
k1ttywh1t9 <143000764+k1ttywh1t9@users.noreply.github.com>
2024-04-24 11:34:40 +0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -2,7 +2,7 @@ from pytube import YouTube
 from title_builder import clear_title
 def downloader(stream, title, extension):
     filename = f'{clear_title(title)}.{extension}'
-    output_path = 'media\\'
+    output_path = 'media/'
     path = f'{output_path}{filename}'
     stream.download(output_path=output_path,filename=filename)
     return path
